18d587e35e55bbfdf77e73594e8da2b5fdcdbb99
Melarn <melarn06@gmail.com>
2023-10-29 10:09:27 +0200

    Last one

--- src/main/java/me/melarn/untitled/HideNickName.java
+++ src/main/java/me/melarn/untitled/HideNickName.java
@@ -2,10 +2,12 @@ package me.melarn.untitled;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -16,12 +18,12 @@ public class HideNickName implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractAtEntityEvent event) {
         // Getting the target
-        if (event.getRightClicked() instanceof Player) {
-            Player player = (Player) event.getRightClicked();
-            Player viewer = event.getPlayer();
+        Player player = event.getPlayer();
 
-            // Show the nickname in the ActionBar when the viewer presses RMB.
-            viewer.sendActionBar("Player: " + player.getName());
+        // Проверка, что игрок зажимает клавишу Shift
+        if (player.isSneaking()) {
+            String playerName = event.getRightClicked().getName(); // Получение ника игрока
+            player.sendActionBar(ChatColor.UNDERLINE + playerName); // Отправка сообщения в ActionBar
         }
     }
 }
--- src/main/java/me/melarn/untitled/Test.java
+++ src/main/java/me/melarn/untitled/Test.java
@@ -2,7 +2,7 @@ package me.melarn.untitled;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class GetPlayerName extends JavaPlugin {
+public final class Test extends JavaPlugin {
 
     @Override
     public void onEnable() {
